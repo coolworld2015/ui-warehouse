d24b7d26d3e4db8cf5ce498af0e2e1133ad2c4d0
wintermute2015 <wintermute2015@ukr.net>
2015-10-28 18:25:05 +0200

    [outputs] - added OutputsInvoice_Del_LocalStorage

--- app/src/outputs/outputsInvoiceDialogCtrl.js
+++ app/src/outputs/outputsInvoiceDialogCtrl.js
@@ -5,9 +5,11 @@
         .module('app')
         .controller('OutputsInvoiceDialogCtrl', OutputsInvoiceDialogCtrl);
 
-    OutputsInvoiceDialogCtrl.$inject = ['$state', '$rootScope', 'OutputsInvoiceService', 'OutputsService', 'GoodsService', 'ClientsService', '$stateParams'];
+    OutputsInvoiceDialogCtrl.$inject = ['$state', '$rootScope', 'OutputsInvoiceService', 'OutputsService',
+        'OutputsInvoiceLocalStorage', 'GoodsService', 'ClientsService', '$stateParams'];
 
-    function OutputsInvoiceDialogCtrl($state, $rootScope, OutputsInvoiceService, OutputsService, GoodsService, ClientsService, $stateParams) {
+    function OutputsInvoiceDialogCtrl($state, $rootScope, OutputsInvoiceService, OutputsService,
+        OutputsInvoiceLocalStorage, GoodsService, ClientsService, $stateParams) {
         var vm = this;
 
         angular.extend(vm, {
@@ -35,39 +37,57 @@
             $stateParams.item.total = outputItem.total;
             var sum = parseFloat($stateParams.invoice.total);
 
-            OutputsInvoiceService.deleteItem(vm.id)
-                .then(function () {
+            if ($rootScope.mode == 'ON-LINE (Heroku)') {
+                OutputsInvoiceService.deleteItem(vm.id)
+                    .then(function () {
 
-                    OutputsService.editItem(outputItem)
-                        .then(function () {
+                        OutputsService.editItem(outputItem)
+                            .then(function () {
 
-                            GoodsService.findGood($stateParams.invoice.goodsID)
-                                .then(function (good) {
-                                    good.data.quantity = parseFloat(good.data.quantity) + parseFloat(vm.quantity);
+                                GoodsService.findGood($stateParams.invoice.goodsID)
+                                    .then(function (good) {
+                                        good.data.quantity = parseFloat(good.data.quantity) + parseFloat(vm.quantity);
 
-                                    GoodsService.editItem(good.data)
-                                        .then(function () {
+                                        GoodsService.editItem(good.data)
+                                            .then(function () {
 
-                                            ClientsService.findClient($stateParams.item.clientID)
-                                                .then(function (client) {
-                                                    client.data.sum = parseFloat(client.data.sum) + parseFloat(sum);
+                                                ClientsService.findClient($stateParams.item.clientID)
+                                                    .then(function (client) {
+                                                        client.data.sum = parseFloat(client.data.sum) + parseFloat(sum);
 
-                                                    ClientsService.editItem(client.data)
-                                                        .then(function () {
-                                                            $rootScope.myError = false;
-                                                            $state.go('main.outputs-invoice', {item: $stateParams.item});
-                                                        })
-                                                        .catch(errorHandler);
-                                                })
-                                                .catch(errorHandler);
-                                        })
-                                        .catch(errorHandler);
+                                                        ClientsService.editItem(client.data)
+                                                            .then(function () {
+                                                                $rootScope.myError = false;
+                                                                $state.go('main.outputs-invoice', {item: $stateParams.item});
+                                                            })
+                                                            .catch(errorHandler);
+                                                    })
+                                                    .catch(errorHandler);
+                                            })
+                                            .catch(errorHandler);
 
-                                }).catch(errorHandler);
-                        })
-                        .catch(errorHandler);
-                })
-                .catch(errorHandler);
+                                    }).catch(errorHandler);
+                            })
+                            .catch(errorHandler);
+                    })
+                    .catch(errorHandler);
+            } else {
+                OutputsInvoiceLocalStorage.deleteItem(vm.id);
+
+//                var id = $scope.invoiceRecordID;
+//                InputInvoiceService.deleteItem(id);
+//
+//                $scope.total = parseFloat($scope.total) - parseFloat($scope.invoiceTotal);
+//                $scope.inputSubmitTotal();
+//
+//                inputTransaction.setClientSum($scope.clientID, -$scope.invoiceTotal);
+//                inputTransaction.setStoreSum($scope.goodsID, -$scope.invoiceQuantity);
+//
+//                $scope.sum = parseFloat($scope.total).toFixed(2);
+//                $rootScope.view = 'inputEditForm';
+
+                $state.go('main.outputs-invoice', {item: $stateParams.item});
+            }
         }
 
         function goOutputsInvoice() {
