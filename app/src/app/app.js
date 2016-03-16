58bc1332a368d36351345380be10d71626b92a02
coolworld2015 <wintermute2015@ukr.net>
2016-03-16 18:21:42 +0200

    [cordova] - updated cordova

--- app/src/app/app.js
+++ app/src/app/app.js
@@ -67,7 +67,7 @@
             //webUrl: 'http://localhost:3000/file/' //TODO Local JSON DB
         };
 
-        //$rootScope.mode = 'OFF-LINE (LocalStorage)'; //TODO !!! ONLY for Google Market
+        $rootScope.mode = 'OFF-LINE (LocalStorage)'; //TODO !!! ONLY for Google Market
         //$rootScope.mode = 'ON-LINE (Heroku)'; //TODO !!! ONLY for Web Site - change index.html (idly-user-logout) and $stateChangeStart
     }
 })();
